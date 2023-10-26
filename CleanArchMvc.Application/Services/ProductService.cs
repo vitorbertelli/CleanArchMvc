b2dd67c6a02c999ad2e573a898ea0952da7d58c2
Vitor <vitorbertelli19@gmail.com>
2023-10-26 10:02:49 -0300

    Implmentando interfaces e serviços para Product e Category

---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services;
+
+public class ProductService : IProductService
+{
+    private IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository ??
+            throw new ArgumentNullException(nameof(productRepository));
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ProductDTO>> GetProducts()
+    {
+        var productsEntity = await _productRepository.GetProductsAsync();
+        return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+    }
+
+    public async Task<ProductDTO> GetById(int? id)
+    {
+        var productEntity = await _productRepository.GetByIdAsync(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+
+    public async Task<ProductDTO> GetProductCategory(int? id)
+    {
+        var productEntity = await _productRepository.GetProductCategoryAsync(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+
+    public async Task Create(ProductDTO productDto)
+    {
+        var productEntity = _mapper.Map<Product>(productDto);
+        await _productRepository.CreateAsync(productEntity);
+    }
+
+    public async Task Update(ProductDTO productDto)
+    {
+        var productEntity = _mapper.Map<Product>(productDto);
+        await _productRepository.UpdateAsync(productEntity);
+    }
+
+    public async Task Remove(int? id)
+    {
+        var productEntity = _productRepository.GetProductCategoryAsync(id).Result;
+        await _productRepository.RemoveAsync(productEntity);
+    }
+}
