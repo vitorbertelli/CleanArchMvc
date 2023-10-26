1d011e72cf946a6be34f757cea804a45f59b0d6e
Vitor <vitorbertelli19@gmail.com>
2023-10-26 11:11:06 -0300

    Registrando serviços e criando Controllers e Views para teste

--- CleanArchMvc.Application/DTOs/CategoryDTO.cs
+++ CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace CleanArchMvc.Application.DTOs
+namespace CleanArchMvc.Application.DTOs;
+
+public class CategoryDTO
 {
-    internal class CategoryDTO
-    {
-    }
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The Name is Requered!")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
 }
