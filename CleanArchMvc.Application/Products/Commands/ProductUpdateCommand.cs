6280aebb6024194f0255536716c28e69b1b390ff
Vitor <vitorbertelli19@gmail.com>
2023-10-27 10:37:59 -0300

    Implementando padrão CQRS para Product

---
+++ CleanArchMvc.Application/Products/Commands/ProductUpdateCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchMvc.Application.Products.Commands;
+
+public class ProductUpdateCommand : ProductCommand
+{
+	public int Id { get; set; }
+	public ProductUpdateCommand(int id)
+	{
+		Id = id;
+	}
+}
--- CleanArchMvc.Application/Services/ProductService.cs
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -1,56 +1,59 @@
 ﻿using AutoMapper;
 using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
-using CleanArchMvc.Domain.Entities;
-using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Application.Products.Queries;
+using MediatR;
 
 namespace CleanArchMvc.Application.Services;
 
 public class ProductService : IProductService
 {
-    private IProductRepository _productRepository;
-    private readonly IMapper _mapper;
+	private readonly IMediator _mediator;
+	private readonly IMapper _mapper;
 
-    public ProductService(IProductRepository productRepository, IMapper mapper)
-    {
-        _productRepository = productRepository ??
-            throw new ArgumentNullException(nameof(productRepository));
-        _mapper = mapper;
-    }
+	public ProductService(IMediator mediator, IMapper mapper)
+	{
+		_mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+		_mapper = mapper;
+	}
 
-    public async Task<IEnumerable<ProductDTO>> GetProducts()
-    {
-        var productsEntity = await _productRepository.GetProductsAsync();
-        return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
-    }
+	public async Task<IEnumerable<ProductDTO>> GetProducts()
+	{
+		var productsQuery = new GetProductsQuery() ?? throw new Exception("Entity could not be loaded.");
+		var result = await _mediator.Send(productsQuery);
+		return _mapper.Map<IEnumerable<ProductDTO>>(result);
+	}
 
-    public async Task<ProductDTO> GetById(int? id)
-    {
-        var productEntity = await _productRepository.GetByIdAsync(id);
-        return _mapper.Map<ProductDTO>(productEntity);
-    }
+	public async Task<ProductDTO> GetById(int? id)
+	{
+		var productsQuery = new GetProductByIdQuery(id.Value) ?? throw new Exception("Entity could not be loaded.");
+		var result = await _mediator.Send(productsQuery);
+		return _mapper.Map<ProductDTO>(result);
+	}
 
-    public async Task<ProductDTO> GetProductCategory(int? id)
-    {
-        var productEntity = await _productRepository.GetProductCategoryAsync(id);
-        return _mapper.Map<ProductDTO>(productEntity);
-    }
+	public async Task<ProductDTO> GetProductCategory(int? id)
+	{
+		var productsQuery = new GetProductByIdQuery(id.Value) ?? throw new Exception("Entity could not be loaded.");
+		var result = await _mediator.Send(productsQuery);
+		return _mapper.Map<ProductDTO>(result);
+	}
 
-    public async Task Create(ProductDTO productDto)
-    {
-        var productEntity = _mapper.Map<Product>(productDto);
-        await _productRepository.CreateAsync(productEntity);
-    }
+	public async Task Create(ProductDTO productDto)
+	{
+		var productCreateCommand = _mapper.Map<ProductCreateCommand>(productDto);
+		await _mediator.Send(productCreateCommand);
+	}
 
-    public async Task Update(ProductDTO productDto)
-    {
-        var productEntity = _mapper.Map<Product>(productDto);
-        await _productRepository.UpdateAsync(productEntity);
-    }
+	public async Task Update(ProductDTO productDto)
+	{
+		var productUpdateCommand = _mapper.Map<ProductUpdateCommand>(productDto);
+		await _mediator.Send(productUpdateCommand);
+	}
 
-    public async Task Remove(int? id)
-    {
-        var productEntity = _productRepository.GetProductCategoryAsync(id).Result;
-        await _productRepository.RemoveAsync(productEntity);
-    }
+	public async Task Remove(int? id)
+	{
+		var productRemoveCommand = new ProductRemoveCommand(id.Value) ?? throw new Exception("Entity could not be loaded.");
+		await _mediator.Send(productRemoveCommand);
+	}
 }
